ae6a75f406c20c718c16a42282da4dc9c9c99963
Sahil Rajput <rajputsahil.2204@gmail.com>
2021-06-30 22:06:26 +0530

    Home app created and basic migration applied

--- Django-CRUD/Django_CRUD/Django_CRUD/settings.py
+++ Django-CRUD/Django_CRUD/Django_CRUD/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home',
+
+
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'Django_CRUD.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

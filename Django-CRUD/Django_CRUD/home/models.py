a77a62e8888d0ce11aa32199d087cdd8ff977932
Sahil Rajput <rajputsahil.2204@gmail.com>
2021-07-05 22:20:48 +0530

    User model created and migrations applied on DB

--- Django-CRUD/Django_CRUD/home/models.py
+++ Django-CRUD/Django_CRUD/home/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    userName = models.CharField(max_length=50)
+    email = models.EmailField()
+    mobileNumber = models.CharField(max_length=10)
+    address = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.userName
